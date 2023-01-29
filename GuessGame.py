d6defc8146e38ea9deedecb3e59d5c3d3049ba2c
OGOLA-SOSPETER <captain483sos@gmail.com>
2023-01-29 23:44:56 +0300

    A python program to compete the system . Who is mightier? Here we go😎😁💻

---
+++ GuessGame.py
@@ -0,0 +1,35 @@
+# A reminder on the random module
+import random
+#creating a class to handle the random functioning
+
+class Guesses:
+    @staticmethod
+    def get_random():
+        attempts = eval(input('Enter the number of times you want to attempt the game: '))
+        counter = attempts
+        for a in range(0,attempts):
+            print('\n','-'*30)
+            number = eval(input("Enter your Guess.\n"))
+            counter = counter - 1
+            guess = random.randint(1, 1000)
+            if number == guess:
+                print("Your Guess is right!"
+                      "\nYou won!")
+                break
+
+            else:
+                print("Wrong Guess!\n"
+                      "System won!")
+                print('The correct answer is ', guess)
+                print('-' * 30)
+
+        new_count = counter
+        print('-' * 30)
+        print('-' * 30)
+        print("You guessed right at the count of {:2d} \ {:2d}".format(new_count,attempts))
+        print('Your percentage pass = {:.4f}'.format(new_count / attempts * 100), '%')
+        print('-' * 30)
+        print('-' * 30)
+
+Ges1 = Guesses
+Ges1.get_random()
---
+++ README.md
@@ -0,0 +1,17 @@
+This is a project done in python to express some basic concepts that have been 
+learnt in the language.
+
+It is a basic game that allows you to enter the number of times you want to game and 
+allows you to make the guesses.
+
+An error is thrown every time you make a wrong guess.
+the system thus wins.
+
+The program then calculates you final percentage score based on the attempts made.
+
+Fun, Right?
+
+Give it a try
+
+@CaptainDevelopers.
+@FlyWayBookers.
